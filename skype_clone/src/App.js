e7bf754bff7b6f0fe018a8bcdddb92859bc4d7b4
Adrian Viquez <aviquez96@gmail.com>
2018-11-10 21:05:30 -0800

    Initial setup of skypey

---
+++ skype_clone/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Sidebar } from './Sidebar.js'
+import { Main } from './Main.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hey
+        {/* <Sidebar />
+        <Main /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ skype_clone/src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Styles/Sidebar.css";
+
+const Sidebar = () => {
+  return <aside className="Sidebar">Sidebar</aside>;
+};
+
+export default Sidebar;
