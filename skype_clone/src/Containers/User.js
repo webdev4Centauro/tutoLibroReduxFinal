6a0244c937ec29894186c29ff590ce8de05e804d
Adrian Viquez <aviquez96@gmail.com>
2018-12-11 22:37:26 -0800

    dispatch action implementation WIP

--- skype_clone/src/Containers/User.js
+++ skype_clone/src/Containers/User.js
@@ -1,18 +1,26 @@
-import React from "react"
-import "../Styles/User.css"
+import React from "react";
+import "../Styles/User.css";
+import store from "../Store/index.js";
+import { setActiveUserId } from "../Actions/index.js";
 
-const User = ({user}) => {
-    const { name, profile_pic, status } = user;
+const User = ({ user }) => {
+  const { name, profile_pic, status } = user;
 
-    return (
-        <div className="User">
-            <img src={profile_pic} alt={name} className="User__pic"/>
-            <div className="User__details">
-                <p className="User__details-name">{name}</p>
-                <p className="User__details-status">{status}</p>
-            </div>
-        </div>
-    )
-}
+  //eventually, try and convert it to arrow function
+  function handleUserClick({ user_id }) {
+    console.log(user_id);
+    store.dispatch(setActiveUserId(user_id));
+  }
 
-export default User
+  return (
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
+      <img src={profile_pic} alt={name} className="User__pic" />
+      <div className="User__details">
+        <p className="User__details-name">{name}</p>
+        <p className="User__details-status">{status}</p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
