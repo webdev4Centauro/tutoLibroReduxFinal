7f9907ebc951347b0f4ffd03141a096157c9c25e
Adrian Viquez <aviquez96@gmail.com>
2018-12-17 21:48:09 -0800

    styling

--- skype_clone/src/Reducer/messages.js
+++ skype_clone/src/Reducer/messages.js
@@ -1,5 +1,5 @@
 import { getMessages } from "../static-data.js";
 
-export default function messages(state = getMessages(10), action) {
+export default function messages(state = getMessages(20), action) {
   return state;
 }
