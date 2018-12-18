27ad9f38810a8f087b5202553cfaaa3d3625e40c
Adrian Viquez <aviquez96@gmail.com>
2018-12-17 21:41:22 -0800

    render specific chat message per usier through activeUserId

--- skype_clone/src/Components/ChatWindow.js
+++ skype_clone/src/Components/ChatWindow.js
@@ -1,9 +1,22 @@
 import React from "react";
+import store from "../Store/index.js";
+import Header from "./Header.js";
+import Chats from "./Chats.js";
+import _ from "lodash";
+import '../Styles/ChatWindow.css'
 
 const ChatWindow = ({ activeUserId }) => {
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+  const activeMessages = state.messages[activeUserId];
+
   return (
-    <div className="ChatWindow">Conversation for user id: {activeUserId}</div>
+    <div className="ChatWindow">
+      <Header user={activeUser} />
+      {/* Pass object as an array of values */}
+      <Chats messages={_.values(activeMessages)}/>
+    </div>
   );
 };
 
-export default ChatWindow
+export default ChatWindow;
--- skype_clone/src/Reducer/index.js
+++ skype_clone/src/Reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import user from "./user.js";
 import contacts from "./contacts.js";
 import activeUserId from "./activeUserId.js"
+import messages from './messages.js'
 
 export default combineReducers({
   user: user,
   contacts: contacts,
-  activeUserId: activeUserId
+  activeUserId: activeUserId,
+  messages: messages
 });
