5cdb23fc018772224b0ab9ecf9c382db9c14f65c
Adrian Viquez <aviquez96@gmail.com>
2018-12-21 09:19:32 -0800

    implemented input submit form functionality with texts being added to the chat window

--- skype_clone/src/Reducer/messages.js
+++ skype_clone/src/Reducer/messages.js
@@ -1,10 +1,25 @@
 import { getMessages } from "../static-data.js";
-import { SEND_MESSAGE } from "../Constants/action-types.js"
+import { SEND_MESSAGE } from "../Constants/action-types.js";
+import _ from "lodash";
 
 export default function messages(state = getMessages(20), action) {
   switch (action.type) {
     case SEND_MESSAGE:
-      return "";
+      const { message, userId } = action.payload;
+      const allUserMsgs = state[userId];
+      const number = +_.keys(allUserMsgs).pop() + 1;
+      return {
+        ...state,
+        [userId]: {
+          ...allUserMsgs,
+          [number]: {
+            number,
+            text: message,
+            is_user_msg: true
+          }
+        }
+      };
+
     default:
       return state;
   }
--- skype_clone/src/Reducer/typing.js
+++ skype_clone/src/Reducer/typing.js
@@ -1,9 +1,11 @@
-import { SET_TYPING_VALUE } from "../Constants/action-types";
+import { SET_TYPING_VALUE, SEND_MESSAGE } from "../Constants/action-types";
 
 export default function typing(state = "", action) {
   switch (action.type) {
     case SET_TYPING_VALUE:
       return action.payload;
+    case SEND_MESSAGE:
+      return "";
     default:
       return state;
   }
